d95b09b623b545ed62415d0cbd8052e1ff910c5d
MahabubArafat <arafatmahabub37@gmail.com>
2020-08-02 14:18:46 +0600

    chapter 3: Templating Engine

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -9,7 +10,7 @@ def index():
     user = {"username": "jack"}
     posts = [
         {"author": {"username": "John"}, "body": "Good Morning"},
-        {"author": {"username": "Wick"}, "body": "I gonna kill you"},
+        {"author": {"username": "Wick"}, "body": "I am gonna kill you"},
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
 
@@ -30,4 +31,15 @@ def index():
 # """
 
 # mane ''' ei 3ta die onek boro string o return kora jay''' jantam e na bal
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f" Log in Complete {form.username.data} and remeber you={form.remember_me.data}"
+        )  # that .data is important cause without oita form theke ja pabe pura ta dekhabe, just username ta grab korbe na
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign In", form=form)
 

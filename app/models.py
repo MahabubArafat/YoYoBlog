7568cafca89c0f962cba0b8bb7afd459d8fda30e
MahabubArafat <arafatmahabub37@gmail.com>
2020-08-08 12:04:15 +0600

    final commit db

--- app/models.py
+++ app/models.py
@@ -17,6 +17,7 @@ class Post(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(300))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    # look here we just passed in the function,but not the result of the function's calling or executed result, cause when here something is passed as default , the sqlalchemy will automatically execute it
     user_id = db.Column(db.Integer, db.ForeignKey("user._id"))
 
     def __repr__(self):
--- microblog.py
+++ microblog.py
@@ -4,6 +4,7 @@ from app.models import Post, User
 
 @app.shell_context_processor
 def make_shell_context():
+    # so that we dont need to import app or db or User or Post classes everytime we open a flask shell
     return {"db": db, "User": User, "Post": Post}
 
 

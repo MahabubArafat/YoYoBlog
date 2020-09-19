a18545c7846825e95c8fa0aa45cea528b80cd40d
MahabubArafat <arafatmahabub37@gmail.com>
2020-09-19 22:05:35 +0600

    some minor fixes

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +15,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"  # url_for nijei lagay ney ei function
 # ar login bolte login function ta re bujaise jeita routes e dewa
+mail = Mail(app)
 
 if not app.debug:
     if app.config["MAIL_SERVER"]:

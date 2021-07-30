994a8edf78b367bf115a71c0a7f39f64685a8139
MahabubArafat <arafatmahabub37@gmail.com>
2021-07-30 18:20:02 +0600

    User Notifications added

--- app/main/routes.py
+++ app/main/routes.py
@@ -14,7 +14,7 @@ from flask_babel import _, get_locale
 from guess_language import guess_language
 from app import db
 from app.main.forms import EditProfileForm, PostForm, SearchForm, MessageForm
-from app.models import User, Post, Message
+from app.models import Notification, User, Post, Message
 from app.translate import translate
 from app.main import bp
 
@@ -199,6 +199,7 @@ def send_message(recipient):
     form = MessageForm()
     if form.validate_on_submit():
         msg = Message(author=current_user, recipient=user, body=form.message.data)
+        user.add_notification("unread_message_count", user.new_messages())
         db.session.add(msg)
         db.session.commit()
         flash("Sent")
@@ -212,6 +213,7 @@ def send_message(recipient):
 @login_required
 def messages():
     current_user.last_message_read_time = datetime.utcnow()
+    current_user.add_notification("unread_message_count", 0)
     db.session.commit()
     page = request.args.get("page", 1, type=int)
     messages = current_user.messages_received.order_by(
@@ -230,5 +232,21 @@ def messages():
         title="Messages",
         next_url=next_url,
         prev_url=prev_url,
+    )
+
+
+@bp.route("/notifications")
+@login_required
+def notifications():
+    since = request.args.get("since", 0.0, type=float)
+    notifications = current_user.notifications.filter(
+        Notification.timestamp > since
+    ).order_by(Notification.timestamp.asc())
+
+    return jsonify(
+        [
+            {"name": n.name, "data": n.get_data(), "timestamp": n.timestamp}
+            for n in notifications
+        ]
     )
 

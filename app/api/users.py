d92ae32d1372e9da204850420c6632ba349862be
MahabubArafat <arafatmahabub37@gmail.com>
2021-09-04 11:31:08 +0600

    api: follower and followed user list

--- app/api/users.py
+++ app/api/users.py
@@ -12,19 +12,27 @@ def get_user(id):
 @bp.route('/users',methods=['GET'])
 def get_users():
     page=request.args.get('page',1,type=int)
-    per_page=min(request.args.get('per_page',10,type=int),100)
+    per_page=min(request.args.get('per_page',20,type=int),100)
     data=User.to_collection_dict(User.query,page,per_page,'api.get_users')
     return jsonify(data)
 
 
 @bp.route('/users/<int:id>/followers',methods=['GET'])
 def get_followers(id):
-    pass
+    user=User.query.get_or_404(id)
+    page=request.args.get('page',1,type=int)
+    per_page=min(request.args.get('per_page',10,type=int),100)
+    data=User.to_collection_dict(user.followers,page,per_page,'api.get_followers',id=id)
+    return jsonify(data)
 
 
 @bp.route('/users/<int:id>/followed',methods=['GET'])
 def get_followed(id):
-    pass
+    user=User.query.get_or_404(id)
+    page=request.args.get('page',1,type=int)
+    per_page=min(request.args.get('per_page',10,type=int),100)
+    data=User.to_collection_dict(user.followed,page,per_page,'api.get_followed',id=id)
+    return jsonify(data)
 
 
 @bp.route('/users',methods=['POST'])

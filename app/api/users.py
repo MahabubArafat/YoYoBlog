dea2b23601a43a9a20241980f57bdaf3c610751f
MahabubArafat <arafatmahabub37@gmail.com>
2021-09-16 20:22:24 +0600

    API:User registration added

--- app/api/users.py
+++ app/api/users.py
@@ -1,7 +1,9 @@
-from flask import jsonify
+from flask import jsonify,url_for
 from flask import request
+from app import db
 from app.models import User
 from app.api import bp 
+from app.api.errors import bad_request
 
 
 @bp.route('/users/<int:id>',methods=['GET'])
@@ -37,7 +39,21 @@ def get_followed(id):
 
 @bp.route('/users',methods=['POST'])
 def create_user():
-    pass
+    data = request.get_json() or {}
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return bad_request('must include username, email and password to register')
+    if User.query.filter_by(username=data['username']).first():
+        return bad_request("please use a different username, this one is taken")
+    if User.query.filter_by(email=data['email']).first():
+        return bad_request("Email already in use")
+    user=User()
+    user.from_dict(data,new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    response=jsonify(user.to_dict())
+    response.status_code=201
+    response.headers['Location']=url_for('api.get_user',id=user.id)
+    return response
 
 
 @bp.route('/users/<int:id>',methods=['PUT'])
